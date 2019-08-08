c210f46f781962c995af6625a54f9f20005dd802
James Serrels <39858908+xXKnight-OwlXx@users.noreply.github.com>
2019-08-08 15:57:43 -0400

    Update Game v2.0.py

--- Game v2.0.py
+++ Game v2.0.py
@@ -16,6 +16,7 @@ FPS = 30
 #colors
 BLACK = (0,0,0)
 WHITE = (255,255,255)
+RED = (255,0,0)
 
 #initilize and create window
 pygame.init()
@@ -31,7 +32,7 @@ class Battleship(pygame.sprite.Sprite):
         super().__init__()
 
         #scale player image
-        self.image = pygame.transform.scale(battleship_img, (100, 100))
+        self.image = pygame.transform.scale(battleship_img, (60, 60))
         self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
 
@@ -60,23 +61,23 @@ class Battleship(pygame.sprite.Sprite):
         if(self.rect.y < 0) or (self.rect.y > 600 - 16 ):
             self.speed_y *= -1
 
-        self.rect.x = self.rect.x + self.speed_x
-        self.rect.y = self.rect.y + self.speed_y
+        #self.rect.x = self.rect.x + self.speed_x
+        #self.rect.y = self.rect.y + self.speed_y
 
         #calculate coordinates
         global Bx
         Bx = self.rect.x
         global By
         By = self.rect.y
-        coordinates_battleship = (Bx,By)
-        #print("Battleship Position:",coordinates_battleship)
+        distanceBP = math.sqrt((Bx - Px)**2 + (By - Py)**2)
+        print("Distance_P2B:",distanceBP)
 
 class Carrier(pygame.sprite.Sprite):
     def __init__(self, carrier_img, all_active_sprites):
         super().__init__()
 
         #scale player image
-        self.image = pygame.transform.scale(carrier_img,(100,100))
+        self.image = pygame.transform.scale(carrier_img,(60,60))
         self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
 
@@ -103,17 +104,19 @@ class Carrier(pygame.sprite.Sprite):
         self.rect.y = self.rect.y + self.speed_y
 
         #calculate coordinates
-        #x = self.rect.x
-        #y = self.rect.y
-        #coordinates_carrier = (x,y)
-        #print("Carrier Position:",coordinates_carrier)
+        global Cx
+        Cx = self.rect.x
+        global Cy
+        Cy = self.rect.y
+        distanceCP = math.sqrt((Cx - Px)**2 + (Cy - Py)**2)
+        print("Distance_P2C:",distanceCP)
 
 class Destroyer(pygame.sprite.Sprite):
     def __init__(self, destroyer_img, all_active_sprites):
         super().__init__()
 
         #scale player image
-        self.image = pygame.transform.scale(destroyer_img,(100,100))
+        self.image = pygame.transform.scale(destroyer_img,(50,50))
         self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
 
@@ -146,16 +149,18 @@ class Destroyer(pygame.sprite.Sprite):
             self.speed_y *= -1
 
         #calculate coordinates
-        #x = self.rect.x
-        #y = self.rect.y
-        #coordinates_destroyer = (x,y)
-        #print("Destroyer Position:",coordinates_destroyer)
+        global Dx
+        Dx = self.rect.x
+        global Dy
+        Dy = self.rect.y
+        distanceDP = math.sqrt((Dx - Px)**2 + (Dy - Py)**2)
+        print("Distance_P2D:",distanceDP)
 
 class Fishing(pygame.sprite.Sprite):
     def __init__(self, fishing_img, all_active_sprites):
         super().__init__()
         #scale player image
-        self.image = pygame.transform.scale(fishing_img, (50, 50))
+        self.image = pygame.transform.scale(fishing_img, (30, 30))
         self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
 
@@ -166,11 +171,11 @@ class Fishing(pygame.sprite.Sprite):
         self.radius = random.randrange(2, 5)
 
         #player starting location
-        self.center_x = random.randrange(100,500)
-        self.center_y = random.randrange(200,700)
+        self.center_x = random.randrange(100,800)
+        self.center_y = random.randrange(200,400)
 
         #how fast will orbit be
-        self.speed = random.choice([-0.05,0.05])
+        self.speed = random.choice([-0.03,0.03])
 
     def update(self):
         ''' update the Fishing class '''
@@ -187,17 +192,19 @@ class Fishing(pygame.sprite.Sprite):
         self.rect.y = self.radius * math.cos(self.angle) + self.center_y
 
         #calculate coordinates
-        #x = self.rect.x
-        #y = self.rect.y
-        #coordinates_fishing = (x,y)
-        #print("Fishing Position:",coordinates_fishing)
+        global Fx
+        Fx = self.rect.x
+        global Fy
+        Fy = self.rect.y
+        distanceFP = math.sqrt((Fx - Px)**2 + (Fy - Py)**2)
+        print("Distance_P2F:",distanceFP)
 
 class Sub(pygame.sprite.Sprite):
     def __init__(self, sub_img, all_active_sprites):
         super().__init__()
 
         #scale player image
-        self.image = pygame.transform.scale(sub_img, (100,100))
+        self.image = pygame.transform.scale(sub_img, (40,40))
         self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
 
@@ -230,16 +237,19 @@ class Sub(pygame.sprite.Sprite):
         self.rect.y = self.rect.y + self.speed_y
 
         #calculate coordinates
-        #x = self.rect.x
-        #y = self.rect.y
-        #coordinates_sub = (x,y)
-        #print("Sub Position:",coordinates_sub)
+        global Sx
+        Sx = self.rect.x
+        global Sy 
+        Sy = self.rect.y
+        #coordinates_sub = (Sx,Sy)
+        distanceSP = math.sqrt((Sx - Px)**2 + (Sy - Py)**2)
+        print("Distance_P2S:",distanceSP)
 
 class Player(pygame.sprite.Sprite):
     def __init__(self,player_img,all_active_sprites):
         super().__init__()
         #scale player image
-        self.image = pygame.transform.scale(player_img, (25, 25))
+        self.image = pygame.transform.scale(player_img, (20, 20))
         self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
 
@@ -287,23 +297,22 @@ class Player(pygame.sprite.Sprite):
         Px = self.rect.x
         global Py 
         Py = self.rect.y
-        coordinates_player = Px,Py
-        #print("Player Position:",coordinates_player)
+        #print("Player Position:",Px,Py)
 
 #main program loop
 def main():
-       
-    #load all game images
-    background = pygame.image.load('/home/knightowl/Documents/EKKO/ocean.jpg').convert_alpha()
+    
+    #load all images
+    background = pygame.image.load('/home/knightowl/EKKO/ocean.jpg').convert_alpha()
     background_rect = background.get_rect()
-    player_img = pygame.image.load('/home/knightowl/Documents/EKKO/player.png').convert_alpha()
+    player_img = pygame.image.load('/home/knightowl/EKKO/player.png').convert_alpha()
     player_img.set_colorkey(BLACK)
     player_rect = player_img.get_rect(center=(WINDOWWIDTH / 2, WINDOWHEIGHT / 2))
-    battleship_img = pygame.image.load('/home/knightowl/Documents/EKKO/battleship.png').convert_alpha()
-    carrier_img = pygame.image.load('/home/knightowl/Documents/EKKO/carrier.png').convert_alpha()
-    destroyer_img = pygame.image.load('/home/knightowl/Documents/EKKO/destroyer.png').convert_alpha()
-    sub_img = pygame.image.load('/home/knightowl/Documents/EKKO/sub.png').convert_alpha()
-    fishing_img = pygame.image.load('/home/knightowl/Documents/EKKO/fishing.png').convert_alpha()
+    battleship_img = pygame.image.load('/home/knightowl/EKKO/battleship.png').convert_alpha()
+    carrier_img = pygame.image.load('/home/knightowl/EKKO/carrier.png').convert_alpha()
+    destroyer_img = pygame.image.load('/home/knightowl/EKKO/destroyer.png').convert_alpha()
+    sub_img = pygame.image.load('/home/knightowl/EKKO/sub.png').convert_alpha()
+    fishing_img = pygame.image.load('/home/knightowl/EKKO/fishing.png').convert_alpha()
 
     #create a list of all sprites
     all_active_sprites = pygame.sprite.Group()
@@ -327,8 +336,8 @@ def main():
     sub2 = Sub(sub_img, all_active_sprites)
     sub3 = Sub(sub_img, all_active_sprites)
     ships = [battleship1, carrier1, destroyer1, sub1,
-             battleship2, carrier2, destroyer2, sub2,
-             battleship3, carrier3, destroyer3, sub3]
+            battleship2, carrier2, destroyer2, sub2,
+            battleship3, carrier3, destroyer3, sub3]
 
     #randomly spawn ships
     for i in range(2):
@@ -339,9 +348,7 @@ def main():
     for i in range(3):
         new_ship = random.sample(ships,1)
         all_active_sprites.add(new_ship)
-        all_active_sprites.add(new_ship)
         print(new_ship)
-
 
     #process input/output events
     while True: # main game loop
@@ -357,17 +364,15 @@ def main():
         #draw background image to game
         WINDOW.blit(background, background_rect)
         all_active_sprites.draw(WINDOW)
+        #draw crosshairs 
+        pygame.draw.circle(WINDOW,RED,(Px+12,Py+12),150,2)
+        pygame.draw.circle(WINDOW,RED,(Px+12,Py+12),100,2)
+        pygame.draw.circle(WINDOW,RED,(Px+12,Py+12),50,2)
         #done after drawing everything to the screen
         pygame.display.flip()
         #limit to 30 FPS
         FPSCLOCK.tick(FPS)
         ticks = pygame.time.get_ticks()
-
-
-        print("Player Position:",Px,Py)
-        print("Battleship Position:",Bx,By)
-        distanceBP = math.sqrt((Bx - Px)**2 + (By - Py)**2)
-        print("Distance:",distanceBP)
 
 if __name__ == "__main__":
     main()
