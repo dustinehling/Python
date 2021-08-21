3ea18553c92cca052e9d66be2ec0b5ba7f76de4f
Dustin Ehling <dustinehling16@gmail.com>
2021-08-21 14:49:26 -0400

    add script

---
+++ BikeShare.py
@@ -0,0 +1,21 @@
+"""
+Script to look into bike share programs in multiple municipalities
+"""
+#%%
+
+import folium
+import pandas as pd
+
+
+#NEW YORK CITY
+nyc_map = folium.Map(location=[40.738,-73.980], zoom_start=13, tiles="CartoDB dark_matter")
+
+#add data
+url = ''
+
+
+
+
+
+nyc_map
+# %%
